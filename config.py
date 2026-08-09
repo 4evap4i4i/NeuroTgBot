6d818caf165724ee2885a7332d463fb35d36e79d
4evap4i4i <chebupelya76@gmail.com>
2026-08-09 23:08:10 +0300

    бывает

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from aiogram import Bot
 from dotenv import load_dotenv
 
 load_dotenv()
-
 Bot = Bot(token=os.getenv("TOKEN"))
-webhook = os.getenv("WEBHOOK")
+db_url = os.getenv("NEON")
+webhook = os.getenv("WEBHOOk")
+render_url = os.getenv("RENDER_EXTERNAL_URL")
